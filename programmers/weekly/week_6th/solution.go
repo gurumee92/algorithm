b15c357e57884a9b2359f8f0fb2e28384b7e17b6
gurumee92 <gurumee92@gmail.com>
2021-09-22 10:13:43 +0900

    code: Add solution*.go

---
+++ programmers/weekly/week_6th/solution.go
@@ -0,0 +1,11 @@
+package week_6th
+
+type Player struct {
+	weight          int
+	winPercent      int
+	winBiggerThanMe int
+}
+
+func solution(weights []int, head2head []string) []int {
+	return []int{}
+}
