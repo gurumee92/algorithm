5ddf5831bdd2fab7848531c0f75d409917198430
1101320 <gurumee92@gmail.com>
2021-10-12 07:50:11 +0900

    #1 9주차 패키지 초기화

---
+++ programmers/weekly/week_9th/README.md
@@ -0,0 +1,15 @@
+# \[ALGORITHM\] 전력망을 둘로 나누기
+
+## 문제
+
+이 문서에서 다룰 문제 "전력망을 둘로 나누기"는 "프로그래머스 > 코딩테스트 연습 > 위클리 챌린지 > 9주차" 문제이다. 문제 링크는 다음과 같다.
+
+* [https://programmers.co.kr/learn/courses/30/lessons/86971](https://programmers.co.kr/learn/courses/30/lessons/86971)
+
+## 문제 풀이
+
+먼저 문제의 입력은 다음과 같다.
+
+
+## 코드
+
---
+++ programmers/weekly/week_9th/solution.py
@@ -0,0 +1,16 @@
+"""
+프로그래머스(programmers.co.kr) 문제 풀이
+
+코딩테스트 연습 > 위클리 챌린지 > 9주차_전략망을_둘로_나누기(https://programmers.co.kr/learn/courses/30/lessons/86971) 문제 풀이
+"""
+def solution(n, wires):
+    """
+    문제 풀이
+    입력:
+    * n:     int
+    * sizes: [][]int
+    출력:
+    * answer: int
+    """
+    answer = 0
+    return answer
