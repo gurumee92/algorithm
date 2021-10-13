f95b576629441ce11f08aa893e6eb68f005b47c8
1101320 <gurumee92@gmail.com>
2021-10-13 09:04:50 +0900

    #5. Update README

--- programmers/weekly/week_9th/solution.py
+++ programmers/weekly/week_9th/solution.py
@@ -8,17 +8,14 @@ MAX = 100
 
 def dfs(cur, num, is_visit, graph):
     """
-    DFS로 시작 노드에서 연결된 모든 노드의 최대 개수를 반환한다.
+    DFS로 시작 노드에서 연결된 모든 노드를 방문
     입력:
     * cur:     int
     * num:     int
     * is_visit []boolean
     * graph:   [][]int
-    출력:
-    * cnt: int
     """
     is_visit[cur] = True
-    cnt = is_visit.count(True)
     is_rest = False
 
     for _next in range(num):
@@ -27,19 +24,17 @@ def dfs(cur, num, is_visit, graph):
             break
 
     if not is_rest:
-        return cnt
+        return
 
     for _next in range(num):
         if graph[cur][_next] == 1 and not is_visit[_next]:
-            cnt = max(cnt, dfs(_next, num, is_visit, graph))
-
-    return cnt
+            dfs(_next, num, is_visit, graph)
 
 
 def solution(n, wires):
     """
     문제 풀이
-    1. 그래프 초기화 
+    1. 그래프 초기화
     2. wires를 순회하여 1개씩 끊은 후 DFS로 연결된 개수 파악, 분할된 네트워크 차이의 최솟값을 구함.
     입력:
     * n:      int
@@ -61,7 +56,8 @@ def solution(n, wires):
 
         for cur in range(n):
             is_visit = [False for _ in range(n)]
-            path_a = dfs(cur, n, is_visit, graph)
+            dfs(cur, n, is_visit, graph)
+            path_a = is_visit.count(True)
             path_b = n - path_a
             diff = abs(path_a - path_b)
             answer = min(answer, diff)
